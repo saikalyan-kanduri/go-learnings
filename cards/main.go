4104c2ef743990e9a0239a04049288f9b1d8f199
Kalyan Kanduri <kalyan@Air-M1.local>
2025-09-13 09:39:09 +0530

    Variable Declaration & Assignment

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var card string = "Ace of Spades"
+	fmt.Println(card)
+
+	firstCard := "Five of Diamonds"
+	fmt.Println(firstCard)
+
+	firstCard = "Two of Hearts"
+	fmt.Println(firstCard)
 }
